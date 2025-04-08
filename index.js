c7896e5574430003a0b937b2490cce095c2c4750
Khavan <krajput123@hotmail.com>
2025-04-08 09:21:13 +0100

    add dotenv file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+const port = process.env.PORT
+
 const app = require('./app')
-const port = 3000
+
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
